07b6dba4cad2a651c07eed577fa4056a1b4311e8
faulty404 <anishmahadevan1772@gmail.com>
2025-04-04 13:29:22 +0530

    Added flex sensor detection code

--- Gloves_ESP32/flex.py
+++ Gloves_ESP32/flex.py
@@ -1,12 +1,29 @@
 from machine import ADC, Pin
 import time
 
-# Setup the flex sensor on an ADC pin (e.g., GPIO34)
-flex_sensor = ADC(Pin(34))  # Use an ADC1-compatible pin
-flex_sensor.atten(ADC.ATTN_11DB)  # Full range: 0-3.3V
+flex_sensor_thumb = ADC(Pin(32)) 
+flex_sensor_index = ADC(Pin(33)) 
+flex_sensor_mid = ADC(Pin(34)) 
+flex_sensor_ring = ADC(Pin(35)) 
+flex_sensor_pinky = ADC(Pin(25)) 
+
+flex_sensor_thumb.atten(ADC.ATTN_11DB) 
+flex_sensor_index.atten(ADC.ATTN_11DB) 
+flex_sensor_mid.atten(ADC.ATTN_11DB) 
+flex_sensor_ring.atten(ADC.ATTN_11DB)
+flex_sensor_pinky.atten(ADC.ATTN_11DB)
+
 
 while True:
-    sensor_value = flex_sensor.read()  # Read the analog value (0-4095)
-    voltage = sensor_value * (3.3 / 4095)  # Convert ADC reading to voltage
-    print("Flex Sensor Value:", sensor_value, "| Voltage:", round(voltage, 2), "V")
+    sensor_value_thumb = flex_sensor_thumb.read()
+    sensor_value_index = flex_sensor_index.read()
+    sensor_value_mid = flex_sensor_mid.read()  
+    sensor_value_ring = flex_sensor_ring.read()
+    sensor_value_pinky = flex_sensor_pinky.read()
+    print("Thumb Sensor Value:", sensor_value_thumb)
+    print("Index Sensor Value:", sensor_value_index)
+    print("Middle Sensor Value:", sensor_value_mid)
+    print("Ring Sensor Value:", sensor_value_ring)
+    print("Pinky Sensor Value:", sensor_value_pinky)
     time.sleep(0.5)
+
